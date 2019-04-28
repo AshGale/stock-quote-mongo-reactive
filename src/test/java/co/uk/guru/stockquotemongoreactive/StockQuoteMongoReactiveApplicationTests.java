1e49b658c04b29a0be151e9fa89dd0b6e22f5114
agale2 <ash.gale@aexp.com>
2019-04-28 16:19:52 +0100

    Initial Commit

---
+++ src/test/java/co/uk/guru/stockquotemongoreactive/StockQuoteMongoReactiveApplicationTests.java
@@ -0,0 +1,16 @@
+package co.uk.guru.stockquotemongoreactive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockQuoteMongoReactiveApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
